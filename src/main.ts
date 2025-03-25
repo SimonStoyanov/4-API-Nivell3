0523a353e7307850e5cd21aa234772544ff8681a
Simón Stoyanov Beltrán <simon.stoyanov@hotmail.com>
2025-03-25 12:34:51 +0100

    made fetchJoke a async function && create interface for the return type

--- src/main.ts
+++ src/main.ts
@@ -6,30 +6,31 @@ const options = {
     }
 }
 
+interface JsonRes {
+    id: string,
+    joke: string,
+    status: number
+}
+
 const button: HTMLButtonElement | null = document.querySelector<HTMLButtonElement>('#nextJoke');
 const joke: HTMLElement = document.querySelector<HTMLParagraphElement>('#joke')!;
 
-button?.addEventListener('click', _fetchJoke => {
+button?.addEventListener('click', getJoke);
+
+async function fetchJoke(): Promise<JsonRes> {
+    const res = await fetch('https://icanhazdadjoke.com/', options);
+    if (!res.ok) return Promise.reject("Failed to fetch dad joke!");
+    return await res.json();
+}
+
+function getJoke(): void {
     fetchJoke()
         .then((data) => {
-            joke.innerHTML = `${data.joke}`
-            console.log(data);
+        joke.innerHTML = `${data.joke}`
+        console.log(data);
         })
         .catch((error) => console.log(error))
-});
-
-function fetchJoke(): Promise<{id: string, joke: string, status: number}> {
-    return fetch('https://icanhazdadjoke.com/', options)
-        .then((res) => {
-            if (!res.ok) return Promise.reject("Failed to fetch dad joke!");
-            return res.json();
-        })
 }
 
 // Create joke at init
-fetchJoke()
-    .then((data) => {
-        joke.innerHTML = `${data.joke}`
-        console.log(data);
-    })
-    .catch((error) => console.log(error))
+getJoke();
